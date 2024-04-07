aa1b8daa967dcca562c7291b60a3c8194b9f8a6c
PaulMcGinley <41758581+PaulMcGinley@users.noreply.github.com>
2024-04-07 10:10:58 +0100

    Added an elapsed time tracker to track function speed and compare it to alternative functions to gauge the fastest way of achieving an output

---
+++ Engine/Trackers/ElapsedTime.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace GU1.Engine.Trackers;
+
+public class ElapsedTime {
+
+    /// <summary>
+    /// Assigning a name to the tracker will make the output more readable
+    /// </summary>
+    private string Name { get; set; }
+
+    private double StartTime { get; set; }
+    private double EndTime { get; set; }
+    private TimeSpan Time => TimeSpan.FromMilliseconds(EndTime - StartTime);
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="name"></param>
+    public ElapsedTime(string name = "Undefined") {
+
+        Name = name;
+        Reset();
+    }
+
+    /// <summary>
+    /// Start tracking the time
+    /// </summary>
+    public void Start() => StartTime = DateTime.Now.TimeOfDay.TotalMilliseconds;
+
+    /// <summary>
+    /// Stop tracking the time
+    /// </summary>
+    public void Stop() => EndTime = DateTime.Now.TimeOfDay.TotalMilliseconds;
+
+    /// <summary>
+    /// Reset the time tracker
+    /// </summary>
+    public void Reset() => StartTime = EndTime = 0;
+
+    /// <summary>
+    /// Get the result of the time tracking
+    /// </summary>
+    public string Result => $"[{Name}] Time elapsed: {Time}";
+
+    /// <summary>
+    /// Override the ToString method to return the result
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => Result;
+
+    /// <summary>
+    /// Compare the time of two ElapsedTime objects
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public int CompareTo(ElapsedTime other) => Time.CompareTo(other.Time);
+
+    /// <summary>
+    /// Check if the object is equal to another ElapsedTime object
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object obj) => obj is ElapsedTime time && Time.Equals(time.Time);
+
+    /// <summary>
+    /// Get the hash code of the time
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode() => HashCode.Combine(Time);
+
+    #region Operators
+
+    public static bool operator <(ElapsedTime a, ElapsedTime b) => a.Time < b.Time;
+    public static bool operator >(ElapsedTime a, ElapsedTime b) => a.Time > b.Time;
+    public static bool operator <=(ElapsedTime a, ElapsedTime b) => a.Time <= b.Time;
+    public static bool operator >=(ElapsedTime a, ElapsedTime b) => a.Time >= b.Time;
+    public static bool operator ==(ElapsedTime a, ElapsedTime b) => a.Time == b.Time;
+    public static bool operator !=(ElapsedTime a, ElapsedTime b) => a.Time != b.Time;
+
+    public static ElapsedTime operator +(ElapsedTime a, ElapsedTime b) => new($"{a.Name} + {b.Name}") { StartTime = a.StartTime, EndTime = b.EndTime };
+    public static ElapsedTime operator -(ElapsedTime a, ElapsedTime b) => new($"{a.Name} - {b.Name}") { StartTime = a.StartTime, EndTime = b.EndTime };
+    public static ElapsedTime operator *(ElapsedTime a, ElapsedTime b) => new($"{a.Name} * {b.Name}") { StartTime = a.StartTime, EndTime = b.EndTime };
+    public static ElapsedTime operator /(ElapsedTime a, ElapsedTime b) => new($"{a.Name} / {b.Name}") { StartTime = a.StartTime, EndTime = b.EndTime };
+    public static ElapsedTime operator %(ElapsedTime a, ElapsedTime b) => new($"{a.Name} % {b.Name}") { StartTime = a.StartTime, EndTime = b.EndTime };
+
+    #endregion
+
+}

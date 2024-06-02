ed6f3a513de67e4b4e1bcff938c3063ff566e6cf
PaulMcGinley <41758581+PaulMcGinley@users.noreply.github.com>
2024-06-02 18:26:51 +0100

    PhotoViewer shows framed photo location

--- Envir/Scenes/PhotoViewer.cs
+++ Envir/Scenes/PhotoViewer.cs
@@ -32,6 +32,15 @@ public class PhotoViewer : IScene {
 
         spriteBatch.Draw(Photo.fullPicture, new Vector2(0, 0), null, Color.White, 0, Vector2.Zero, 0.5f, SpriteEffects.None, 0);
 
+        if (IsGamePadButtonDown(0, Buttons.Y)) {
+
+            Vector2 location = new Vector2(1920, 1080) + new Vector2(Photo.location.X, Photo.location.Y);
+            Vector2 scale = new Vector2(0.5f, 0.5f); // scale factor for 4K to 1080p
+            Vector2 location1080p = Vector2.Multiply(location, scale);
+
+            spriteBatch.Draw(Photo.framedPicture, location1080p - new Vector2(1920/4,1080/4) - new Vector2(64+12.5f,64+12.5f), null, Color.White*0.5f, 0, Vector2.Zero, 0.5f, SpriteEffects.None, 0);
+        }
+
         spriteBatch.End();
     }
 

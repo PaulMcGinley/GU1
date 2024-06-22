4bd885003df5356fe7e1ff7f3f4f47d849ac533c
PaulMcGinley <41758581+PaulMcGinley@users.noreply.github.com>
2024-06-22 13:17:06 +0100

    Added 'now playing' to playing scene when song changes

--- Envir/Scenes/Playing.cs
+++ Envir/Scenes/Playing.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -32,6 +31,12 @@ public class Playing : IScene {
     float touristScoreOpacity = 1f;                                                                         // The opacity of the score bounds
     float touristMinScoreOpacity = 0.3f;                                                                    // The minimum opacity of the score bounds
 
+    Song nowPlaying;
+    string nowPlayingString = "Now Playing: {0}";
+    double hideNowPlaying = 0f;                                                                              // The opacity of the now playing text
+    bool showNowPlaying = true;                                                                              // The flag to show the now playing text
+    double showNowPlayingTime = 0f;                                                                          // The time to show the now playing text
+
     #region IScene Implementation
 
     public void Initialize(GraphicsDevice device) {
@@ -46,7 +51,14 @@ public class Playing : IScene {
         camera.LookAt(new Vector2(1920/2, 1080/2));                                                         // Set the camera to look at the center of the screen
         camera.SetZoomLevel(0.5f);                                                                          // Set the camera zoom level
 
-        MediaPlayer.ActiveSongChanged += (s, e) => MediaPlayer.Play(SLib.GameMusic[random.Int(0, SLib.GameMusic.Length)]); // When the song ends, play another random game music track
+        MediaPlayer.ActiveSongChanged += (s, e) =>
+        {
+            nowPlaying = SLib.GameMusic[random.Int(0, SLib.GameMusic.Length)];
+            MediaPlayer.Play(nowPlaying);
+            showNowPlaying = true;
+            showNowPlayingTime = 0f;
+
+        }; // When the song ends, play another random game music track
     }
 
     public void LoadContent(ContentManager content) { }
@@ -83,6 +95,15 @@ public class Playing : IScene {
 
         foreach (var cloud in GameState.Clouds)
             cloud.Update(gameTime);
+
+        if (showNowPlayingTime == 0f) {
+            showNowPlayingTime = gameTime.TotalGameTime.TotalSeconds;
+            hideNowPlaying = gameTime.TotalGameTime.TotalSeconds + 5f;
+        }
+
+        if (gameTime.TotalGameTime.TotalSeconds > hideNowPlaying)
+            showNowPlaying = false;
+
     }
 
     public void FixedUpdate(GameTime gameTime) {
@@ -204,6 +225,20 @@ public class Playing : IScene {
             spriteBatch.DrawString(FLib.LeaderboardFont, player.CameraView.playerName + " : " +  player.Score.ToString(), new Vector2(touristScoreBounds.Left, 20*touristCount) + Vector2.One, Color.Black * touristScoreOpacity);
             spriteBatch.DrawString(FLib.LeaderboardFont, player.CameraView.playerName + " : " +  player.Score.ToString(), new Vector2(touristScoreBounds.Left, 20*touristCount), Color.White * touristScoreOpacity);
             touristCount++;
+        }
+
+        spriteBatch.End();
+
+        spriteBatch.Begin();
+
+        string song = nowPlaying.Name.Replace("-", " ");
+        // Proper string
+        int nowPlayingWidth = (int)FLib.LeaderboardFont.MeasureString(string.Format(nowPlayingString, song)).X;
+        int nowPlayingHeight = (int)FLib.LeaderboardFont.MeasureString(string.Format(nowPlayingString, song)).Y;
+        // Now Playing
+        if (showNowPlaying) {
+            spriteBatch.DrawString(FLib.LeaderboardFont, string.Format(nowPlayingString, song), new Vector2((1920/2) - (nowPlayingWidth/2),  + 15) + Vector2.One, Color.Black * 0.5f);
+            spriteBatch.DrawString(FLib.LeaderboardFont, string.Format(nowPlayingString, song), new Vector2((1920/2) - (nowPlayingWidth/2),  + 15), Color.White);
         }
 
         spriteBatch.End();
@@ -370,7 +405,8 @@ public class Playing : IScene {
     private void SetupBackgroundMusic() {
 
         MediaPlayer.Volume = GameState.MusicVolume;                                                         // Set the volume of the music player
-        MediaPlayer.Play(SLib.GameMusic[random.Int(0, SLib.GameMusic.Length)]);                             // Play a random game music track
+        nowPlaying = SLib.GameMusic[random.Int(0, SLib.GameMusic.Length)];                                  // Set the now playing song to a random game music track
+        MediaPlayer.Play(nowPlaying);                                                                       // Play a random game music track
     }
 
     private void CreateNewClouds() {
